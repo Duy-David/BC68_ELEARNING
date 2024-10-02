e16554bbe1c47cb5059a6f4b23e9292deeb207f6
CongKTI <congkti@gmail.com>
2024-10-02 16:13:02 +0700

    done homepage

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -1,68 +1,73 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import "./banner.scss";
+import FormSearchBar from "../FormSearchBar/FormSearchBar";
+import IconBell from "../Icon/IconBell";
 
 const Banner = () => {
   return (
-    <div className="container">
-      Banner xxx <FontAwesomeIcon icon={faDownload} />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non nobis
-        aspernatur earum facere iure expedita, neque sequi quaerat vel vitae
-        corrupti enim quisquam aliquid autem corporis, laudantium asperiores
-        doloremque. Tempora suscipit harum velit animi labore voluptatibus.
-        Quisquam et assumenda dicta doloribus dolore in modi sed dignissimos.
-        Repudiandae beatae eos animi aut. Non alias quos labore id modi nobis
-        eaque. Maxime veniam in illum ea voluptates placeat? Beatae aspernatur
-        ipsum tempore culpa quibusdam omnis, animi minima veritatis. Incidunt
-        dignissimos molestias provident fugit, rem id? Praesentium aut
-        accusantium nulla quae et autem odio consectetur culpa quam laborum
-        perspiciatis fugiat accusamus tenetur, magni nostrum nesciunt! Iste
-        atque iure veritatis exercitationem impedit quisquam quas voluptatum ad
-        aperiam esse! Delectus quo eaque sunt autem nostrum, blanditiis voluptas
-        pariatur quae at laboriosam maiores tempora dicta iure, non impedit eum
-        qui. Aliquid perferendis magnam commodi architecto eligendi quia quae
-        repudiandae totam quidem obcaecati error odio ea vitae, reprehenderit,
-        officia quos delectus atque. Similique accusamus dignissimos enim,
-        possimus tenetur aliquid. Corporis maiores, officiis dicta, quo quaerat
-        itaque reiciendis facere fugit saepe libero ullam, iusto voluptatum
-        consequatur! Est illum rem reiciendis in deserunt et ex at laboriosam
-        tempore eligendi. Laboriosam eos corporis molestias cumque vero minus
-        reprehenderit fugiat excepturi repellat omnis impedit quibusdam sit
-        tempora sed laborum facilis, at ducimus numquam odio optio dolores
-        dignissimos sapiente. Nihil blanditiis quam voluptas iure mollitia porro
-        tempore quos? Officiis nobis accusamus error deserunt a in architecto
-        ipsam quasi atque commodi! Aliquam numquam, laborum voluptatem possimus
-        perferendis minus ex explicabo sit facere sed aliquid quidem excepturi
-        rerum molestiae porro itaque doloremque, nam illum placeat error at
-        dignissimos consequuntur libero nobis! Mollitia laudantium quis sint
-        quo, iure quia sit natus nemo, impedit voluptatibus magni sequi ea alias
-        consectetur officiis doloremque pariatur harum enim suscipit! Qui quae
-        veritatis labore fuga, placeat earum tenetur rerum tempora assumenda
-        saepe. Alias in illum quaerat vitae nobis amet sunt eveniet facere
-        deserunt deleniti recusandae veritatis, officiis tenetur eum ullam vel
-        excepturi explicabo aperiam rerum aliquid rem corporis animi officia!
-        Laborum nulla maxime sit quis recusandae aliquid suscipit sequi
-        repellendus, quod nemo rerum ad, in eaque possimus, explicabo magni
-        dolor dolore maiores et accusantium exercitationem! Magni accusamus
-        repellendus sit nemo amet possimus neque nam doloremque minus tempore.
-        Eos voluptatum accusamus perspiciatis velit dignissimos! Laboriosam rem
-        iste commodi cupiditate iure unde numquam eaque illo sed eveniet, quo
-        laborum a modi dolorem eius enim quas praesentium aliquid labore
-        recusandae sunt veniam amet. Nam, sed animi minima eos, fugit alias id
-        reprehenderit aspernatur, sunt optio quae itaque consectetur. Error
-        omnis, totam veritatis nobis vero rerum a repellendus velit fugiat
-        asperiores! Deleniti nobis quasi, laborum facilis iure eius pariatur ad
-        temporibus labore consequatur ut, quos corrupti. Veritatis ipsum, amet
-        iste earum saepe, pariatur soluta at beatae optio officiis sed eaque
-        quaerat incidunt assumenda laboriosam expedita quas, alias ad
-        perferendis nulla dolorum ipsa officia. Alias neque maxime similique
-        laborum, debitis veniam aliquid dicta id exercitationem libero assumenda
-        laboriosam provident rem reiciendis. Totam saepe porro nisi sed
-        voluptatum in numquam necessitatibus similique impedit error, quia at!
-      </p>{" "}
-    </div>
+    <section className="slider-section px-3 relative">
+      <div className="container mx-auto pb-10" id="scene">
+        <div className="banner_content flex items-center flex-wrap">
+          <div className="banner_left w-full md:w-1/2">
+            <div className="slider-content px-4">
+              <p className="text-base font-bold uppercase text-[#0071dc] mb-3">
+                Start to success
+              </p>
+              <h2 className="text-4xl leading-tight mb-6">
+                Access To <mark>5500+</mark> Courses from <mark>480</mark>{" "}
+                Instructors & Institutions
+              </h2>
+              <p className="text-xl mb-10">
+                Take your learning organisation to the next level.
+              </p>
+              <div className="banner_search mb-8">
+                <FormSearchBar />
+              </div>
+            </div>
+          </div>
+
+          <div className="banner_right w-full md:w-1/2">
+            <div className="slider-image px-4">
+              <div className="relative">
+                <img src="assets/home-01-hero-image.png" alt="" />
+                <div className="absolute -bottom-6 left-0 p-5 bg-white shadow-2xl rounded-lg max-w-[255px]">
+                  <IconBell className="w-14 h-14 p-4 rounded-full bg-[#ffc221] absolute -top-8 -left-7 icon-white" />
+                  <p>
+                    Tomorrow is our{" "}
+                    <strong className="font-medium">
+                      "When I Grow Up" Spirit Day!
+                    </strong>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <img
+          class="slider-section__shape-01"
+          data-depth="0.8"
+          src="assets/edumall-shape-grid-dots.png"
+          alt="Shape"
+          width="417"
+          height="371"
+        />
+        <div class="slider-section__shape-02" data-depth="-1"></div>
+        <div class="slider-section__shape-03" data-depth="1.6"></div>
+        <img
+          class="slider-section__shape-04"
+          data-depth="-0.6"
+          src="assets/edumall-shape-01.png"
+          alt="Shape"
+          width="179"
+          height="178"
+        />
+      </div>
+    </section>
   );
 };
 
 export default Banner;
+
+// https://matthew.wagerfield.com/parallax/
+// https://github.com/wagerfield/parallax
