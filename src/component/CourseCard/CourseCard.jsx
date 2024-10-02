cae551f1068c3a55bd50602d1e256842555555c1
Duy-David <duydavid2506@gmail.com>
2024-10-02 14:57:47 +0700

    update code 02/10/2024

--- src/component/CourseCard/CourseCard.jsx
+++ src/component/CourseCard/CourseCard.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { limitCharacter } from "../../util/util";
 import { Popover } from "antd";
@@ -81,9 +81,9 @@ const CourseCard = ({ course }) => {
     </div>
   );
   return (
-    <Popover content={content}>
-      <div className=" rounded-lg overflow-hidden shadow-lg bg-white">
-        {/* Course Image */}
+    <div className=" rounded-lg overflow-hidden shadow-lg bg-white">
+      {/* Course Image */}{" "}
+      <Popover content={content}>
         <div className="relative">
           <img
             className="w-full h-auto md:h-48"
@@ -95,39 +95,38 @@ const CourseCard = ({ course }) => {
             Featured
           </span>
         </div>
+      </Popover>
+      {/* Course Content */}
+      <div className="p-5">
+        {/* Course Level */}
+        <span className="inline-block bg-yellow-100 text-yellow-600 text-xs font-semibold px-2 py-1 rounded-full">
+          Beginner
+        </span>
 
-        {/* Course Content */}
-        <div className="p-5">
-          {/* Course Level */}
-          <span className="inline-block bg-yellow-100 text-yellow-600 text-xs font-semibold px-2 py-1 rounded-full">
-            Beginner
-          </span>
-
-          {/* Course Title */}
-          <h3 className="text-sm font-bold mt-2 mb-1 min-h-max sm:min-h-11">
-            <Link to={`/course-catelogies/detail-course/${course.maKhoaHoc}`}>
-              {limitCharacter(course.tenKhoaHoc, 50)}
-            </Link>
-          </h3>
-
-          {/* Course Instructor */}
-          <p className="text-sm font-medium mb-2 min-h-max sm:min-h-10">
-            Teacher: {limitCharacter(course.nguoiTao.hoTen, 37)}
-          </p>
-
-          {/* Course view */}
-          <p className="text-lg font-bold text-green-500">
-            {" "}
-            Viewer: {course.luotXem}
-          </p>
+        {/* Course Title */}
+        <h3 className="text-sm font-bold mt-2 mb-1 min-h-max sm:min-h-11">
           <Link to={`/course-catelogies/detail-course/${course.maKhoaHoc}`}>
-            <button className="btn btn-primary w-full mt-2">
-              Course details
-            </button>
+            {limitCharacter(course.tenKhoaHoc, 50)}
           </Link>
-        </div>
+        </h3>
+
+        {/* Course Instructor */}
+        <p className="text-sm font-medium mb-2 min-h-max sm:min-h-10">
+          Teacher: {limitCharacter(course.nguoiTao.hoTen, 37)}
+        </p>
+
+        {/* Course view */}
+        <p className="text-lg font-bold text-green-500">
+          {" "}
+          Viewer: {course.luotXem}
+        </p>
+        <Link to={`/course-catelogies/detail-course/${course.maKhoaHoc}`}>
+          <button className="btn btn-primary w-full mt-2">
+            Course details
+          </button>
+        </Link>
       </div>
-    </Popover>
+    </div>
   );
 };
 
