2abafc96fd25d7a31712504d05a71be66ee26734
HAI LE <nhuthai.le@gmail.com>
2024-10-08 10:20:25 +0700

    template conflict update

--- src/template/Admintemplate/AdminTemplate.jsx
+++ src/template/Admintemplate/AdminTemplate.jsx
@@ -8,11 +8,11 @@ import {
 } from "@ant-design/icons";
 import { Button, Layout, Menu, theme } from "antd";
 import { getLocalStorage } from "../../util/util";
-import { useDispatch,  } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 const { Header, Sider, Content } = Layout;
 const AdminTemplate = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   const handleBackToHomePage = () => {
     navigate("/");
   };
@@ -21,84 +21,85 @@ const AdminTemplate = () => {
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
-  
+
   useEffect(() => {
     let dataLocal = getLocalStorage("user");
-<<<<<<< Updated upstream
     console.log(dataLocal);
-    dataLocal.maLoaiNguoiDung  !== "GV"
-=======
+    dataLocal.maLoaiNguoiDung !== "GV";
     console.log("dataLocal", dataLocal);
     dataLocal.maLoaiNguoiDung !== "GV"
->>>>>>> Stashed changes
       ? (window.location.href = "https://www.google.com/")
       : null;
   }, []);
 
   return (
-    <Layout className="min-h-full">
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="demo-logo-vertical" />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={["1"]}
-          items={[
-            {
-              key: "1",
-              icon: <UserOutlined />,
-              label: <Link to={"/admin/manager-user"}> Quản lý người dùng</Link>,
-            },
-            {
-              key: "2",
-              icon: <VideoCameraOutlined />,
-              label: <Link to={"/admin/manager-course"}> Quản lý Khóa học</Link>,
-            },
-            {
-              key: "3",
-              icon: <UploadOutlined />,
-              label: <Link>Thêm Công Việc</Link>,
-            },
-          ]}
-        />
-      </Sider>
-      <Layout>
-        <Header
-          style={{
-            padding: 0,
-            background: colorBgContainer,
-          }}
-        >
-          <Button
-            type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: "16px",
-              width: 64,
-              height: 64,
-            }}
-          />
-            <Button
-            onClick={handleBackToHomePage}
-            style={{ marginRight: "20px" }}
-          >
-            Back to HomePage
-          </Button>
-        </Header>
-        <Content
-          style={{
-            margin: "24px 16px",
-            padding: 24,
-            minHeight: 280,
-            background: colorBgContainer,
-            borderRadius: borderRadiusLG,
-          }}
-        >
-          <Outlet />
-        </Content>
-      </Layout>
-    </Layout>
+    <WithLoading>
+      <>
+        {" "}
+        <Layout className="min-h-full">
+          <Sider trigger={null} collapsible collapsed={collapsed}>
+            <div className="demo-logo-vertical" />
+            <Menu
+              theme="dark"
+              mode="inline"
+              defaultSelectedKeys={[""]}
+              items={[
+                {
+                  key: "1",
+                  icon: <UserOutlined />,
+                  label: (
+                    <Link to={"/admin/manager-user"}> Quản lý người dùng</Link>
+                  ),
+                },
+                {
+                  key: "2",
+                  icon: <VideoCameraOutlined />,
+                  label: (
+                    <Link to={"/admin/manager-course"}> Quản lý Khóa học</Link>
+                  ),
+                },
+              ]}
+            />
+          </Sider>
+          <Layout>
+            <Header
+              style={{
+                padding: 0,
+                background: colorBgContainer,
+              }}
+            >
+              <Button
+                type="text"
+                icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                onClick={() => setCollapsed(!collapsed)}
+                style={{
+                  fontSize: "16px",
+                  width: 64,
+                  height: 64,
+                }}
+              />
+              <Button
+                onClick={handleBackToHomePage}
+                style={{ marginRight: "20px" }}
+              >
+                Back to HomePage
+              </Button>
+            </Header>
+            <Content
+              style={{
+                margin: "24px 16px",
+                padding: 24,
+                minHeight: 280,
+                background: colorBgContainer,
+                borderRadius: borderRadiusLG,
+              }}
+            >
+              <Outlet />
+            </Content>
+          </Layout>
+        </Layout>
+      </>
+    </WithLoading>
   );
 };
 export default AdminTemplate;
