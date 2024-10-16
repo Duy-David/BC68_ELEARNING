12aff1293a96d3ef70c405b97091a7b11da04355
Duy-David <duydavid2506@gmail.com>
2024-10-16 18:05:50 +0700

    end code

--- src/component/HeaderCategory/HeaderCategory.jsx
+++ src/component/HeaderCategory/HeaderCategory.jsx
@@ -6,11 +6,14 @@ import IconCategory from "../Icon/Iconheaders";
 import useResponsive from "../../hooks/useResponsive";
 
 const HeaderCategory = () => {
-  const isResponsive = useResponsive({
+  const breakpoints = {
+    xs: 468,
     sm: 640,
     md: 768,
     lg: 1024,
-  });
+  };
+
+  const isResponsive = useResponsive(breakpoints);
   const [listCourseCategory, setListCoursCategory] = useState([]);
   useEffect(() => {
     quanLyKhoaHocService
@@ -31,7 +34,28 @@ const HeaderCategory = () => {
       </Link>
     ),
   }));
+  // const renderCategory = () => {
+  //   // // Ẩn ở xs
+  //   // if (isResponsive.xs) {
+  //   //   return null;
+  //   // }
 
+  //   // Hiện từ sm đến md (415px - 768px)
+  //   // if (!isResponsive.xs && isResponsive.md) {
+  //   //   return <span>Category</span>;
+  //   // }
+  //   // if (!isResponsive.md && isResponsive.lg) {
+  //   //   return null;
+  //   // }
+  //   // Hiện khi > lg (lớn hơn 1024px)
+  //   if (!isResponsive.lg
+  //     || (isResponsive.md && !isResponsive.xs)
+  //   ) {
+  //     return <span>Category</span>;
+  //   } else {
+  //     return null;
+  //   }
+  // };
   return (
     <Dropdown
       menu={{
@@ -41,10 +65,20 @@ const HeaderCategory = () => {
       arrow
     >
       <Button>
-      <div className="category_icon flex">
-        <IconCategory />
-        {!isResponsive.lg ? <span>Category</span> : <></>}
-      </div>
+        <div className="category_icon flex">
+          <IconCategory />
+          {isResponsive.xs ? (
+            <></>
+          ) : isResponsive.xs && !isResponsive.md ? (
+            isResponsive.lg ? (
+              <></>
+            ) : (
+              <span>Category</span>
+            )
+          ) : (
+            <span>Category</span>
+          )}
+        </div>
       </Button>
     </Dropdown>
   );
